1ce8b3e1dbae86df5a13b758638ed0cc13482453
fbt <fbtgold@foxmail.com>
2020-11-25 11:14:09 +0800

    第一次上传，胖接口

--- 接口隔离/Program.cs
+++ 接口隔离/Program.cs
@@ -10,6 +10,66 @@ namespace 接口隔离
     {
         static void Main(string[] args)
         {
+            var driver = new Driver(new HeavyTank());
+            driver.work();
+        }
+
+
+    }
+
+    class Driver
+    {
+        public ITank _vehicle;
+
+        public Driver(ITank vehicle)
+        {
+            _vehicle = vehicle;
+        }
+
+        public void work()
+        {
+            _vehicle.Run();
+        }
+    }
+
+    interface IVehicle
+    {
+        void Run();
+    }
+
+    class Car:IVehicle
+    {
+        public void Run()
+        {
+            Console.WriteLine("Car is runing!");
+        }
+    }
+
+    class Truck:IVehicle
+    {
+       public void Run()
+        {
+            Console.WriteLine("Truck is running");
+        }
+    }
+
+    interface ITank
+    {
+        void Run();
+
+        void Fire();
+    }
+
+    class HeavyTank:ITank
+    {
+        public void Run()
+        {
+            Console.WriteLine("Tank is running");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("Boom!");
         }
     }
 }
